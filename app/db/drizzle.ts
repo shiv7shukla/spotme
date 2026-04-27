0ea45ecf5479ec9d63f9e1dc0ac46d38a3183780
Shiv Shukla <shivshukla00514@gmail.com>
2026-04-27 14:38:42 +0530

    fix: db caching and url validation

--- app/db/drizzle.ts
+++ app/db/drizzle.ts
@@ -2,8 +2,13 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import 'dotenv/config';
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) throw new Error("DATABASE URL not set");
+
+const globalForDb = globalThis as unknown as  { _spotmePool?: Pool};
+const pool = globalForDb._spotmePool ?? new Pool({ connectionString });
+
+if (process.env.NODE_ENV !== "production") globalForDb._spotmePool = pool;
 
 export const db = drizzle(pool);
