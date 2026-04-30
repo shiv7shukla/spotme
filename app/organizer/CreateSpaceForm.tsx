a61f7f3b5383529ef38a4302075fde8325de65dd
Shiv Shukla <shivshukla00514@gmail.com>
2026-05-01 03:36:03 +0530

    add: form validation done

--- app/organizer/CreateSpaceForm.tsx
+++ app/organizer/CreateSpaceForm.tsx
@@ -1,48 +1,67 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { spaceSchema } from "../schemas/space.schema";
+
+interface SpaceFormInput {
+    spaceName: string,
+    password: string
+};
 
 const CreateSpaceForm = () => {
-    const [spaceName, setSpaceName] = useState("");
-    const [password, setPassword] = useState("");
+    const { 
+        register, 
+        handleSubmit, 
+        formState:{ errors, isSubmitting, touchedFields } 
+    } = useForm<SpaceFormInput>({
+        defaultValues: {
+            spaceName: "",
+            password: ""
+        },
+        resolver: zodResolver(spaceSchema),
+        mode: "onSubmit"
+    });
 
-    const handleCreateSpace = () => {
-        if (!spaceName.trim() || !password.trim()) return;
-    };
+    const formSubmit = (d: SpaceFormInput) => {
+        console.log(d)
+    }
 
     return (
         <div className="glass-card p-6 sm:p-8">
-        <div className="space-y-5 mb-8">
-            <div>
-            <label className="font-body text-sm font-medium text-foreground block mb-2">Space name</label>
-            <input
-                type="text"
-                value={spaceName}
-                onChange={(e) => setSpaceName(e.target.value)}
-                placeholder="Sarah &amp; Tom&apos;s Wedding"
-                className="w-full h-10 px-3 rounded-lg border border-input bg-background font-body text-sm text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring/20 focus:border-foreground/20 transition-all"
-            />
-            </div>
-            <div>
-            <label className="font-body text-sm font-medium text-foreground block mb-2">Access password</label>
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Something memorable for your guests"
-                className="w-full h-10 px-3 rounded-lg border border-input bg-background font-body text-sm text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring/20 focus:border-foreground/20 transition-all"
-            />
-            </div>
-        </div>
-        <Button
-            size="lg"
-            className="w-full"
-            onClick={handleCreateSpace}
-            disabled={!spaceName.trim() || !password.trim()}
-        >
-            Create Space
-        </Button>
+            <form onSubmit={handleSubmit(formSubmit)}>
+                <div className="space-y-5 mb-8">
+                    <div>
+                        <label className="font-body text-sm font-medium text-foreground block mb-2">Space name</label>
+                        <input
+                            type="text"
+                            {...register("spaceName")}
+                            placeholder="Sarah &amp; Tom&apos;s Wedding"
+                            className="w-full h-10 px-3 rounded-lg border border-input bg-background font-body text-sm text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring/20 focus:border-foreground/20 transition-all"
+                        />
+                        {touchedFields.spaceName && errors.spaceName && (<p className="text-red-500 text-xs sm:text-sm">{String(errors.spaceName?.message)}</p>)}
+                    </div>
+                    <div>
+                        <label className="font-body text-sm font-medium text-foreground block mb-2">Access password</label>
+                        <input
+                            type="password"
+                            {...register("password")}
+                            placeholder="Something memorable for your guests"
+                            className="w-full h-10 px-3 rounded-lg border border-input bg-background font-body text-sm text-foreground placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring/20 focus:border-foreground/20 transition-all"
+                        />
+                        {touchedFields.password && errors.password && (<p className="text-red-500 text-xs sm:text-sm">{String(errors.password?.message)}</p>)}
+                    </div>
+                </div>
+                <Button
+                    size="lg"
+                    className="w-full"
+                    disabled={isSubmitting}
+                >
+                    Create Space
+                </Button>
+            </form>
         </div>
     );
 };
--- app/schemas/space.schema.ts
+++ app/schemas/space.schema.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const spaceSchema = z.object({
-    space_name: z.string().trim().min(1, "Space name is required"),
+    spaceName: z.string().trim().min(1, "Space Name is required"),
     password: z.string().trim().min(5, "Password is too short")
 });
 
